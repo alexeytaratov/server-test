c91dce85bb7f169347483b57a10649db8b90e6a0
alexeytaratov <alexeytaratov@gmail.com>
2021-06-19 23:20:29 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-const dbUrl = 'mongodb+srv://admin:admin000@offerlist.dfj5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const dbUrl = 'mongodb+srv://admin:admin000@offerlist.dfj5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -24,17 +24,17 @@ app.use(function(req, res, next) {
   }
 });
 app.use(express.json({extended: true}));
-app.use('/api/requests', require('./routes/requests.route'));
+// app.use('/api/requests', require('./routes/requests.route'));
 app.use('/api/search', require('./routes/search.route'));
 
 async function start() {
   try {
-    await mongoose.connect(dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: true
-    })
+    // await mongoose.connect(dbUrl, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: true
+    // })
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`)
     })
